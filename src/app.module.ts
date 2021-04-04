26b854c64b7224d17f4fdc9e435be644c84df6a4
Jiwook00 <jwkim775@gmail.com>
2021-04-04 14:28:19 +0900

    learn : #2.2 Movies Service part One - 엔티티와 서비스 만들기

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 //  데코레이터
 @Module({
   imports: [],
   controllers: [AppController, MoviesController],
-  providers: [AppService],
+  providers: [AppService, MoviesService],
 })
 export class AppModule {}
 // 앱 모듈은, 모든 것의 루트 모듈 같은
