ae7d308e8072245717071fb448087cd58d0ed5c6
Jiwook00 <jwkim775@gmail.com>
2021-04-04 14:47:17 +0900

    learn : NestJS #2.4 Movies Service part Two

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -10,11 +10,17 @@ export class MoviesService {
   }
 
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Not fond movie ID: ${id}`);
+      // NestJS 가 제공하는 예외처리, HttpException 에서 확장된 NestJS의 제공 기능
+    }
+    return movie;
   }
 
   deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
+    this.getOne(id); //위에서 만든 예외 처리를 재활용 할 수 있다.!
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
     return true;
   }
 
@@ -23,5 +29,11 @@ export class MoviesService {
       id: this.movies.length + 1,
       ...movieData,
     });
+  }
+
+  update(id: string, updaetData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updaetData });
   }
 }
